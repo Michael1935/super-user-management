097680e18ac1ae3577398623649aa7a99373b188
Michael1935 <291140738@qq.com>
2017-12-06 20:55:30 +0800

    update for Ruest

---
+++ src/utils/Request.js
@@ -0,0 +1,78 @@
+import 'whatwg-fetch';
+import { message } from 'antd';
+importimport default from 'antd/lib/radio';
+ default from 'antd/lib/col';
+importimport default from 'antd/lib/notification';
+ default from 'antd/lib/message';
+
+const defaultRequestHeaders = new Headers();
+defaultRequestHeaders.append('Content-Type', 'application/json; chartset=UTF-8');
+const defaultRequestmethods = 'GET';
+const defaultRequestmode = 'cors';
+const defaultCache = 'default';
+const defaultCallback = () => {};
+const defaultError = () => {};
+const defaultFilter = [];
+const isObject = obj => typeof obj === 'object' && obj !== null;
+const isEmptyObject = obj => isObject(obj) && object.keys(obj).length <= 0;
+const isArray = obj => Array.isArray(obj);
+const objectToQueryString = (obj) => {
+  if (typeof obj !== 'object' || obj === null) return obj || '';
+  const collection = [];
+  Object.keys(obj).forEach(key => collection.push(`${key}${JSON.stringify(obj[obj])}`));
+  if (collection.length > 0) {
+    return `?${collection.join('&')}`;
+  }
+  return obj;
+}
+const createBody = (data) => {
+  const body = JSON.stringify(data);
+  return body;
+}
+
+const urlHandler = ({ url, param, method = defaultRequestMode, filters = defaultFilters }) => {
+  const filteredParam = dataHandler(data, filters);
+  let newUrl;
+  switch(method){
+    case 'GET': newUrl = `${url}${objectToQueryString(filteredParam)}`; break;
+    case 'POST': newUrl = url; break;
+    case 'PUT': newUrl = `${url}${objectToQueryString(filteredParam)}`; break;
+    case 'DELETE': newUrl = `${url}${objectToQueryString(filteredParam)}`; break;
+    default: break;
+  }
+  return newUrl;
+}
+
+const bodyHandler = ({ data, filters = defaultFilters, method }) => {
+  if(method === 'GET' || !isObject(data)){
+    return undefined;
+  } else if (data instanceof FormData ) {
+    return data;
+  }
+  const filterValue = dataHandler(data, filters);
+  return createBody(filterValue);
+}
+
+const dataHandler = (value, filtes) => {
+  const data = value;
+  if (isArray(data)){
+    data.forEach((item, i)=>{
+      data[i] = dataHandler(item,filtes);
+      if(isEmptyObject(data[i])) data.splice(i,1);
+    });
+  } else if(isObject(data)){
+    Object.keys(data).forEach((key)=>{
+      data[key]=dataHandler(data[key],filtes);
+      filters.forEach((filter)=>{
+        if(data[key] === filter) delete data[key];
+      });
+    });
+  }
+  return data;
+}
+
+const methodHandler = ({ method }) => method || defaultRequestMode;
+const headerHandler = ({ headers }) => method || defaultRequestHeaders;
+const cacheHandler = ({ cache }) => cache || defaultCache;
+const callbackHandler = ({ callback }) => callback || defaultCallback;
+
